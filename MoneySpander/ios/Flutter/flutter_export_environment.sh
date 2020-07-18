677e994e0e2d1f9e25494e566a5bfbfc08e46712
s.vuckovic <s.vuckovic@levi9.com>
2020-07-18 17:55:52 +0200

    init

---
+++ MoneySpander/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/s.vuckovic/development/tools/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/s.vuckovic/Desktop/Strahinja/Flutter Projects/Flutter/MoneySpander"
+export "FLUTTER_TARGET=/Users/s.vuckovic/Desktop/Strahinja/Flutter Projects/Flutter/MoneySpander/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/s.vuckovic/development/tools/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
